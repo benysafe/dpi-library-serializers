b53fa345bc4960dfa57361f7eebe8e1f472bd57e
Benigno E. Santos Fernández <b.santos@datys.cu>
2023-08-31 10:42:57 -0400

    version 1.0

---
+++ SerializerDictionaryToAvroGenericRecord/SerializerConfig.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SerializerDictionaryToAvroGenericRecord
+{
+    public class SerializerConfig
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Mssgtype[] mssgtypes { get; set; }
+    }
+
+    public class Mssgtype
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string schemaRegistryUrls { get; set; }
+        public string schemaId { get; set; }
+        public string schemaSubject { get; set; }
+        public Recipient[] recipients { get; set; }
+    }
+
+    public class Recipient
+    {
+        public string publisher_id { get; set; }
+        public string recipient { get; set; }
+    }
+
+}
---
+++ SerializerJsonToJson/SerializerConfig.cs
@@ -0,0 +1,24 @@
+﻿
+namespace SerializerJsonToJson
+{
+    public class SerializerConfig
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Mssgtype[] mssgtypes { get; set; }
+    }
+
+    public class Mssgtype
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Recipient[] recipients { get; set; }
+    }
+
+    public class Recipient
+    {
+        public string publisher_id { get; set; }
+        public string recipient { get; set; }
+    }
+
+}
